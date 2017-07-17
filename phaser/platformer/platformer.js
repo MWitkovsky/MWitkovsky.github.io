1e17fa7800f007f1a9d153b026b4d7d64daf3e2a
Michael Witkovsky <michael.witkovsky@gmail.com>
2017-07-17 13:37:13 -0400

    navbars

--- phaser/platformer/platformer.js
+++ phaser/platformer/platformer.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 var player;
 var cursors;
 var jumpButton;
+var platforms;
 
 function preload(){
     game.load.crossOrigin = true; //Allows us to laod images from places other than phaser's custom sprite library
@@ -15,6 +16,8 @@ function preload(){
 function create(){
     game.add.sprite(0, -450, 'background'); //Adds the sprite called background at coordinates 0, -450
     player = game.add.sprite(0, 540, 'player');
+    platforms = game.add.physicsGroup();
+    
     game.world.setBounds(0, 0, 1920, 600);
     game.physics.arcade.enable(player);
     player.body.gravity.y = 25;
@@ -24,8 +27,20 @@ function create(){
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     leftButton = game.input.keyboard.addKey(Phaser.Keyboard.A);
     rightButton = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    
+    game.camera.follow(player);
 }
 
 function update(){
+    player.body.velocity.x = 0;
     
+    if (cursors.left.isDown || leftButton.isDown){
+        player.body.velocity.x -= 300;
+    }
+    if (cursors.right.isDown || rightButton.isDown){
+        player.body.velocity.x += 300;
+    }   
+    if (jumpButton.isDown && (player.body.touching.down || player.body.blocked.down)){
+        player.body.velocity.y = -350;
+    }
 }
