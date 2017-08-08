5d0307d333fd527368d0caf5d0bc3c3692decfe4
Michael Witkovsky <michael.witkovsky@gmail.com>
2017-08-08 13:43:06 -0400

    phaser tiled platformer

---
+++ media/images/tiles/metroidvania set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="troid_spritesheet16" tilewidth="16" tileheight="16" tilecount="8192" columns="128">
+ <image source="troid_spritesheet16.gif" width="2048" height="1024"/>
+</tileset>
--- phaser/platformer2/platformer2.js
+++ phaser/platformer2/platformer2.js
@@ -4,14 +4,46 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {
     update: update
 });
 
-function preload(){
-    
+var map;
+var platformLayer;
+
+var player;
+
+//Groups
+var enemyGroup;
+
+
+function preload() {
+    game.load.tilemap("tilemap", "level1.json", null, Phaser.Tilemap.TILED_JSON);
+    game.load.image("player", "/media/images/RiseOfTheChaosWizards/redmage.png");
+    game.load.image("tiles", "/media/images/tiles/troid_spritesheet16.gif");
+    game.load.image("enemy", "/media/images/RiseOfTheChaosWizards/blackmage.png");
 }
 
-function create(){
+function create() {
+    game.stage.backgroundColor = "#000000";
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    player = game.add.sprite(50, game.height / 2, "player");
+    game.physics.arcade.enable(player);
+
+    map = game.add.tilemap("tilemap");
+    map.addTilesetImage("troid_spritesheet16", "tiles");
+
+    platformLayer = map.createLayer("platforms");
+    platformLayer.resizeWorld();
     
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 2000;
+    player.body.gravity.x = 20;
+    player.body.collideWorldBounds = true;
+    player.body.drag.set(100);
+    game.camera.follow(player);
+    
+    enemyGroup = game.add.group();
+    enemyGroup.enableBody = true;
 }
 
-function update(){
-    
+function update() {
+
 }
